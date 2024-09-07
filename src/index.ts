06c8c0185f1b5a2dd94092761761c28798e59d61
Adebayo Michael <mcgamma04@yahoo.com>
2024-09-07 16:13:53 +0100

    done with user register

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import userRoutes from "./routes/user.route";
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ if (isNaN(PORT)) {
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/v1/', userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
