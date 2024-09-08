26c0485e513f8200a3a804f5deb66a9b2d3b9ad5
Adebayo Michael <mcgamma04@yahoo.com>
2024-09-08 07:24:03 +0100

    update dto

---
+++ src/dtos/updateUser.dto.ts
@@ -0,0 +1,12 @@
+import { UserRole } from "@prisma/client";
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  name?: string;
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+}
