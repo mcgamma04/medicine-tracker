d1d67fdee1f7bd0d3286c1fa16b7cb5f8f44d419
Adebayo Michael <mcgamma04@yahoo.com>
2024-09-13 04:54:03 +0100

    done with cloudinary config

--- src/config/cloudinart.config.ts
+++ src/config/cloudinart.config.ts
@@ -11,3 +11,31 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
+
+// Use a dynamic function for setting Cloudinary parameters
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: async (req, file) => {
+      return {
+        folder: 'manufacturer-documents', // Define the folder here
+        format: 'png', // Force file format to png (or you can change it based on file type)
+        public_id: `${file.originalname.split('.')[0]}-${Date.now()}`, // Generate unique public ID
+      };
+    },
+  });
+
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+    fileFilter: (req:any, file:any, cb:any) => {
+      if (!file.mimetype.match(/image\/(jpg|jpeg|png)|application\/pdf/)) {
+        cb(new Error('Only image and PDF files are allowed'), false);
+      } else {
+        cb(null, true);
+      }
+    },
+  });
+  
+  export const uploadToCloudinary = upload.array('images', 5); // Allow up to 5 images
