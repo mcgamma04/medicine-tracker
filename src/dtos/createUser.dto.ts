67d01a61df3c25c56257cb38fa367ac4a14edc25
Adebayo Michael <mcgamma04@yahoo.com>
2024-09-30 09:21:20 +0100

    patches

--- src/dtos/createUser.dto.ts
+++ src/dtos/createUser.dto.ts
@@ -1,4 +1,4 @@
-import { UserRole, UserStatus } from "@prisma/client";
+import { UserRole } from "@prisma/client";
 import { IsEmail, IsEnum, IsOptional, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto {
